fa66bda98d0251b81be69c56eea81e3914fd4d09
friiwayer <fxbyden@gmail.com>
2015-11-16 20:40:00 +0300

    Create options.php

---
+++ mod/options.php
@@ -0,0 +1,12 @@
+<?
+/* Db Mysql Options */
+Class Opt {
+    public $_main_dir = '/view';
+    public $_dbname = 'test_u';
+    public $_user = 'root';
+    public $_pass = '';
+    public $_charset = 'UTF-8'; 
+    public $_pref = 'tst_';
+    public $grabl = 'http://cronixinc.com/common/php_test_files/images.txt';
+    public $image_dir = '/images/';
+}
