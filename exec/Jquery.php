30c2d91b63ef3e7f70194d4ac5759d53a1cf31f5
friiwayer <fxbyden@gmail.com>
2015-11-17 05:07:22 +0300

    Update Jquery.php

--- exec/Jquery.php
+++ exec/Jquery.php
@@ -5,6 +5,7 @@ Class Ajax extends Conn
 var $image;
 var $image_type;
 public $post;
+public $type;
 public $grab = 'http://cronixinc.com/common/php_test_files/images.txt';
 
 public function check()
@@ -27,6 +28,17 @@ function remoteFileExists($url){
     }
     curl_close($curl);
     return $ret;
+}
+
+public function post($index){
+if(isset($_POST[$index])){
+if(!empty($_POST[$index])){
+$index = $_POST[$index];
+$index = trim($index);
+$index = htmlspecialchars($index);
+return $index;}
+else return false;
+}else return false;
 }
 
 public function edit_img($array,$dir)
@@ -55,24 +67,24 @@ public function edit_img($array,$dir)
             }
             
             $dest = imagecreatetruecolor($thumb_width,$thumb_height);//600*600 new image
-            $whiteBackground = imagecolorallocate($dest, 255, 255, 255);  
+            $whiteBackground = imagecolorallocate($dest, 255, 255, 255);
             
             if($width > $thumb_width)
             {
             $this->resizeToWidth($thumb_width);
             $src = $this->image;
+            imagefill($dest,0,0,$whiteBackground);            
             imagecopyresampled($dest, $src, 0, 0, 0, 0, 600, 600, $thumb_width, $thumb_height);
             //imagecopy($dest,$src,0, 0, 0, 0, $thumb_width, $thumb_height);
-            imagefill($dest,0,0,$whiteBackground);
             $this->image = $dest;
-            $this->save('..'.$dir.'00'.$s.'.png');
+            $this->save('..'.$dir.'00'.$s.'.jpg',IMAGETYPE_JPEG);
             }elseif($width <= $thumb_width)
             {
             $src = $this->image;
+            imagefill($dest,0,0,$whiteBackground);            
             imagecopy($dest,$src,(($thumb_width - $width)/ 2), (($thumb_height - $height) / 2), 0, 0, $thumb_width, $thumb_height);
-            imagefill($dest,0,0,$whiteBackground);
             $this->image = $dest;
-            $this->save('..'.$dir.'00'.$s.'.png');
+            $this->save('..'.$dir.'00'.$s.'.jpg',IMAGETYPE_JPEG);
             }
          $this->inBase_($array[$s]);   
         }
@@ -88,19 +100,26 @@ public function inBase_($im)
     $this->sql($query);
 }
 
-public function showImg($order='date ASC')
+public function hash_()
 {
+    $url = $_SERVER['REQUEST_URI'];
+    
+}
+
+public function showImg($order='`date` ASC')
+{   
+    $dir = $order =='`date` DESC'?'down':'up';
     $query = 'SELECT * FROM `tst_images` ORDER BY '.$order;
     $select = $this->sql($query);
     if($select)
     {
-    $tr .= '<tr class="panel-heading brtop shdw"><th>#name</th><th>#img</th><th>#path</th><th><a onclick="filtr(this);" href="#">#date</a></th></tr>';
+    $tr .= '<tr class="panel-heading brtop shdw"><th>#name</th><th>#img</th><th>#path</th><th><a onclick="filtr(this);return false;" href="#" class="'.$dir.'">#date <span class="caret-'.$dir.'"></span></a></th></tr>';
     while($table = mysql_fetch_array($select))
     {
     $tr .= '
     <tr>
     <td>/00'.$table['name'].'</td>
-    <td><img width="'.$this->img_w_out.'" height="'.$this->image_h_out.'" src="'.$table['new_path'].'00'.$table['name'].'.png"/></td>
+    <td><img width="'.$this->img_w_out.'" height="'.$this->image_h_out.'" src="'.$table['new_path'].'00'.$table['name'].'.jpg"/></td>
     <td>'.$table['old_path'].'</td>
     <td>'.$table['date'].'</td>
     </tr>';
@@ -114,7 +133,7 @@ public function showImg($order='date ASC')
 private function load($filename) {
       $image_info = getimagesize($filename);
       $this->image_type = $image_info[2];
-      if( $this->image_type == IMAGETYPE_JPEG ) {
+      if($this->image_type == IMAGETYPE_JPEG ) {
          $this->image = imagecreatefromjpeg($filename);
       } elseif( $this->image_type == IMAGETYPE_GIF){
          $this->image = imagecreatefromgif($filename);
@@ -128,8 +147,8 @@ private function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $p
       } elseif( $image_type == IMAGETYPE_GIF ) {
          imagegif($this->image,$filename);
       } elseif( $image_type == IMAGETYPE_PNG ) {
-         $white = imagecolorallocate($this->image, 255, 255, 255);
-         imagefill($this->image,0,0,$white);
+         imagealphablending($this->image, false);
+         imagesavealpha($this->image,true);        
          imagepng($this->image,$filename);
       }
       imagedestroy($this->image);
@@ -158,7 +177,7 @@ private function resizeToHeight($height) {
       $width = $this->getWidth() * $ratio;
       $this->resize($width,$height);
 }
-private function resizeToWidth($width) {
+private function resizeToWidth($width){
       $ratio = $width / $this->getWidth();
       $height = $this->getheight() * $ratio;
       $this->resize($width,$height);
@@ -168,9 +187,22 @@ private function scale($scale) {
       $height = $this->getheight() * $scale/100;
       $this->resize($width,$height);
 }
-public function resize($width,$height) {
-      $new_image = imagecreatetruecolor($width, $height);
+public function resize($width,$height){
+      $new_image = imagecreatetruecolor(600, 600);
+      
+      if(($this->image_type == IMAGETYPE_GIF) || ($this->image_type==IMAGETYPE_PNG)){
+      imagealphablending($new_image, false);
+      imagesavealpha($new_image,true);
+      $transparent = imagecolorallocatealpha($new_image, 255, 255, 255, 127);
+      imagefilledrectangle($new_image, 0, 0, $width, $height, $transparent);
       imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+      }else
+      {
+      $whiteBackground = imagecolorallocate($new_image, 255, 255, 255);
+      imagefill($new_image,0,0,$whiteBackground);
+      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+      }
+      
       $this->image = $new_image;
 }
 ////parse file string
@@ -220,10 +252,7 @@ public function blur($fes,$type,$url)
 $affrade = new Ajax;
 if($affrade->check())
 {
-if($_POST['parse']){
-$affrade->blur('parse',$_POST,$affrade->grab);
-}
-if($_POST['getIm']){ 
-$affrade->showImg();
-}
+if($affrade->post('parse')){$affrade->blur('parse',$_POST,$affrade->grab);}
+if($affrade->post('getIm')){$affrade->showImg();}
+if($affrade->post('filtr')){$ud = $affrade->post('filtr'); $ord = $ud=='up'?'DESC':'ASC';  $affrade->showImg('`date` '.$ord);}
 }
