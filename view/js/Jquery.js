eec72fd02c99b3be34e560e73ff2978fdaa1bca0
friiwayer <fxbyden@gmail.com>
2015-11-17 05:08:22 +0300

    Update Jquery.js

--- view/js/Jquery.js
+++ view/js/Jquery.js
@@ -25,6 +25,16 @@ function load_images()
 $.post("../../exec/Jquery.php",{getIm:1})
 .done(function(data){
 $('.table').html(data);$('.dn').removeClass('dn');
-$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+$("html, body").animate({scrollTop:$(document).height()},1000);
 });
+}
+
+function filtr(vd)
+{
+var now = $(vd).attr('class');
+if(now=='up'){$(vd).removeClass('up').addClass('down').children('span').attr('class','caret-down');window.location.hash = '#down';}
+if(now=='down'){$(vd).removeClass('down').addClass('up').children('span').attr('class','caret-up');window.location.hash = '#up';}
+$.post("../../exec/Jquery.php",{filtr:now}).done(function(data){$('.table').html(data);
+});
+return false;
 }
